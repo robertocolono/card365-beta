aff1cc9d1e5d4c51b8fdf5eaa777a1917bf1b17e
robertocolono <roberto.colono17@gmail.com>
2025-10-09 11:17:17 +0000

    init beta

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+};
+export default nextConfig;
